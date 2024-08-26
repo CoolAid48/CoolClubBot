47d159037099eb05494b529fa2e28cbc1cf2ed77
CoolAid <96690746+CoolAid48@users.noreply.github.com>
2024-08-25 23:03:57 -0400

    Replacing src directory

--- src/commands/prefix/test.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'test',
-    description: 'YUP WE ARE DOING THIS AGAIN FROM SCRATCH SMILE',
-    
-    run: async (client, message, args) => {
-        message.reply("YUP WE ARE DOING THIS AGAIN FROM SCRATCH SMILE")
-    }
-}
--- src/events/ready/consoleLog.js
+++
@@ -1,3 +0,0 @@
-module.exports = (client) => {
-    console.log(`${client.user.username} is online.`);
-};
